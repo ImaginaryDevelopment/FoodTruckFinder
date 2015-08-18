81df0ea845afc74199a0a7c480587b4366f23e20
bdimpe@gmail <bdimpe@gmail>
2015-08-18 12:59:01 -0400

    adding truck options

---
+++ FoodTruckFinder/FoodTruckFinder/Controllers/TruckController.fs
@@ -0,0 +1,14 @@
+﻿namespace FoodTruckFinder.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Web
+open System.Web.Mvc
+open System.Web.Mvc.Ajax
+
+type TruckController() =
+    inherit Controller()
+    member x.Index()= x.View()
+
+
