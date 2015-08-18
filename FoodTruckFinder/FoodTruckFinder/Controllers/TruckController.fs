0f973f362bda7b2b141a0ee581321f82534c4f5b
bdimpe@gmail <bdimpe@gmail>
2015-08-18 13:21:10 -0400

    truck index renders!

--- FoodTruckFinder/FoodTruckFinder/Controllers/TruckController.fs
+++ FoodTruckFinder/FoodTruckFinder/Controllers/TruckController.fs
@@ -1,5 +1,4 @@
 ﻿namespace FoodTruckFinder.Controllers
-
 open System
 open System.Collections.Generic
 open System.Linq
@@ -7,8 +6,19 @@ open System.Web
 open System.Web.Mvc
 open System.Web.Mvc.Ajax
 
+open FoodTruckFinder.ViewModels
+
+module TruckMemoryRepo = 
+    let trucks = [ 
+        {
+            Truck.Calendar= [(DateTime.Now, TimeSpan.FromHours(1.), "Your House")]
+            Truck.Menu=["Spoiled sports $1.54"]
+            Truck.Phone="411-hon"
+        }
+    ]
+
 type TruckController() =
     inherit Controller()
-    member x.Index()= x.View()
+    member x.Index()= x.View(TruckMemoryRepo.trucks)
 
 
